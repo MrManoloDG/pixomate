63995a273886407d8a2b9ba4f642f05f1b1c6723
MrManoloDG <manueldiazgil96@hotmail.com>
2020-09-24 13:52:45 +0200

    refactor: db crecentials with env

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const { Sequelize } = require('sequelize');
+const dbUser = process.env.DB_USER || 'root';
+const dbPassword = process.env.DB_PASSWORD || '';
+const dbHost = process.env.DB_HOST || 'localhost';
 
-const sequelize = new Sequelize('pixomaticmdg', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize('pixomaticmdg', dbUser, dbPassword, {
+    host: dbHost,
     dialect: 'mysql'
 });
 
